c9377578f9ac8949a6bdb88728ff526ad49d3e31
nidhi7485 <110181299+nidhi7485@users.noreply.github.com>
2022-07-28 22:48:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// let count = 0;
+
+// const value = document.getElementById("value");
+// const btns = document.querySelector(".btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListner("click", function (e) {
+//     console.log(e.currentTarget);
+//   });
+// });
+
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else if (count == 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
